49cd3df0227058dd196f5727d3a7ccff91b046fb
Sai Teja <105296022+Sai-teja260@users.noreply.github.com>
2025-05-29 15:00:29 +0200

    Create ollamatest.py

---
+++ ollama/ollamatest.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+response = requests.post(
+    url="http://localhost:11434/api/generate"
+    headers={
+        "Authorization": "abcd",
+        "HTTP-Referer": "<your_site_url>",
+        "X-Title": "<your_site_name>"
+    },
+    data=json.dumps({
+        "model":"llama3.2:1b",
+        "prompt": "Where is Hyderabad located?",
+        "stream":False
+    })
+)
